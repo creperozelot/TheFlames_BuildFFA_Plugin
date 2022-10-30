e49e7ea8dcc53d0b2ceb8abc47037b71d764bbd6
creperozelot <saaarlouis@gmail.com>
2022-10-30 13:52:02 +0100

    - Test

--- src/main/java/theflames/buildffa/utils/Scoreboard.java
+++ src/main/java/theflames/buildffa/utils/Scoreboard.java
@@ -1,12 +1,7 @@
 package theflames.buildffa.utils;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
-import theflames.buildffa.Buildffa;
 
-import java.util.HashMap;
 
 public class Scoreboard implements Listener {
 
